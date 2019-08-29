3fa96fd2feef7cbab2e36121318b786bb33cd367
Issa <knholdings@yahoo.com>
2019-08-29 10:59:06 +0300

    allocation

--- function.py
+++ function.py
@@ -10,4 +10,12 @@ def separation(entry,list, paid_list):
         else:
                 print("entry Not captured")
                
+def allocation(entry,paid_list,officer_list):
+        if entry in paid_list:
+                officer = random.choice(officer_list)
+
+                print("Entry" + str(entry) + "has been allocated to" + officer)
+
+        else:
+                print("Entry hasnt paid yet ")
 
